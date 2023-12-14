355ea870d6aaddd422dd1d401a0000ba11c76d10
owaismujtaba <owais.mujtaba123@gmail.com>
2023-12-14 16:39:29 +0100

    traine

---
+++ src/trainer.py
@@ -0,0 +1,22 @@
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
+import config
+
+def train_model(model, train_laoder, validation_loader):
+    print('Training the model')
+    model.compile(
+        optimizer=Adam(),
+        loss=SparseCategoricalCrossentropy(),
+        metrics=['accuracy']
+    )
+    
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+    history = model.fit(
+        train_laoder,
+        epochs=config.epochs,
+        callbacks=[early_stopping],
+        validation_data=validation_loader
+    )
+    return history
